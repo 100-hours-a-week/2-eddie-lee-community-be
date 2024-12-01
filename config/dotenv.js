ad1127db44b743c0a0e15c7a394812ca73c0e2ce
kjr2020 <kjr202020@gmail.com>
2024-12-01 21:08:12 +0900

    루트디렉토리 동적으로 가져오게 만들기

--- config/dotenv.js
+++ config/dotenv.js
@@ -1,7 +1,14 @@
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const result = dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const rootDirectory = __dirname.split('config')[0];
+process.env.PROJECT_ROOT = rootDirectory;
 
 const env = {
     PORT: process.env.PORT || 3000,
