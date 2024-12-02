76e91ea3ecf7d61b4c6b257fdce2057297c9371f
kjr2020 <kjr202020@gmail.com>
2024-12-02 10:31:44 +0900

    origin 주소 변경

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT;
 const router = express.Router();
 app.use(
     cors({
-        origin: 'http://localhost:4444', // 허용할 도메인
+        origin: 'http://52.78.44.85', // 허용할 도메인
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // 허용할 HTTP 메서드
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true, // 쿠키를 포함한 요청을 허용하려면 true로 설정
