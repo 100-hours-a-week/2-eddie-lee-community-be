0c5bfa65ab44112f5764bb2e581c1baead9fa39c
kjr2020 <kjr202020@gmail.com>
2025-01-13 02:14:41 +0900

    fix: S3저장소 사용 세팅

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,43 +1,84 @@
 import multer from 'multer';
 import env from '../config/dotenv.js';
+import AWS from 'aws-sdk';
 
 const rootDir = env.ROOT_DIR;
 
-const profileImg = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, `${rootDir}/public/images/profileImages`);
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-    },
+const s3 = new AWS.S3({
+    accessKeyId: env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+    region: env.AWS_REGION,
 });
 
-const postImg = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const dir = `${rootDir}/public/images/postImages`;
-        //ensureDir(dir);
-        cb(null, dir);
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-    },
-});
+const storage = multer.memoryStorage();
+
+const uploadProfileImg = async (req, res, next) => {
+    try {
+        await new Promise((resolve, reject) => {
+            setMulter(req, res, err => {
+                if (err) reject(err);
+                else resolve();
+            });
+        });
+
+        const fileName = Date.now() + '-' + req.file.originalname;
+        const s3Response = await uploadToS3(
+            req.file.buffer,
+            'public/images/profileImages',
+            fileName,
+            req.file.mimeType,
+        );
+
+        req.file.location = s3Response.Location;
+    } catch (error) {
+        next(error);
+    }
+};
+
+const uploadPostImg = async (req, res, next) => {
+    try {
+        await new Promise((resolve, reject) => {
+            setMulter(req, res, err => {
+                if (err) reject(err);
+                else resolve();
+            });
+        });
+
+        const fileName = Date.now() + '-' + req.file.originalname;
+        const s3Response = await uploadToS3(
+            req.file.buffer,
+            'public/images/postImages',
+            fileName,
+            req.file.mimeType,
+        );
+
+        req.file.location = s3Response.Location;
+    } catch (error) {
+        next(error);
+    }
+};
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image/')) {
+    if (file.mimetype && file.mimetype.startsWith('image/')) {
         cb(null, true);
     } else {
         cb(new Error('Only image files are allowed!'), false);
     }
 };
 
-const uploadProfileImg = multer({
-    storage: profileImg,
-    fileFilter,
-});
+const uploadToS3 = async (fileBuffer, bucketPath, fileName, mimeType) => {
+    const params = {
+        Bucket: env.AWS_S3_BUCKET_NAME,
+        Key: `${bucketPath}/${fileName}`,
+        Body: fileBuffer,
+        ContentType: mimeType,
+        ACL: 'public-read',
+    };
+    return s3.upload(params).promise();
+};
 
-const uploadPostImg = multer({
-    storage: postImg,
+const setMulter = multer({
+    storage: storage,
     fileFilter,
 });
 
